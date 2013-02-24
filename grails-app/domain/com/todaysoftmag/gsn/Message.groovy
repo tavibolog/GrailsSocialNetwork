6fa9e0f8b822a2f53cb0d239863379236560a0dd
Tavi Bolog <tavi.bolog@gmail.com>
2013-02-24 12:18:31 +0100

    update

--- grails-app/domain/com/todaysoftmag/gsn/Message.groovy
+++ grails-app/domain/com/todaysoftmag/gsn/Message.groovy
@@ -7,10 +7,17 @@ class Message {
 	static belongsTo = [user: User]
 
     static constraints = {
-		message size:5..100, blank:false
+		message size:5..100, blank:false, validator: {val, obj -> obj.messageValidator(val)}
     }
 	
 	String toString() {
 		message + " on " + date + " by " + user
+	}
+	
+	private messageValidator(val) {
+		if (val.toLowerCase().contains("test")) {
+			return "test.is.not.allowed"
+		}
+		return true
 	}
 }
